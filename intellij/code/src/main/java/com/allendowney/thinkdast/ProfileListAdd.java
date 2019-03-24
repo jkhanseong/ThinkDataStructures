9b6986a1c47814a96b5a1b0d6ac98787b60f39ed
HanSeong Lee <jkhanseong@gmail.com>
2019-03-24 20:41:15 +0900

    to Fill in ProfileListAdd class

--- intellij/code/src/main/java/com/allendowney/thinkdast/ProfileListAdd.java
+++ intellij/code/src/main/java/com/allendowney/thinkdast/ProfileListAdd.java
@@ -1,12 +1,11 @@
 package com.allendowney.thinkdast;
 
+import com.allendowney.thinkdast.Profiler.Timeable;
+import org.jfree.data.xy.XYSeries;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-
-import org.jfree.data.xy.XYSeries;
-
-import com.allendowney.thinkdast.Profiler.Timeable;
 
 public class ProfileListAdd {
 	
@@ -14,10 +13,10 @@ public class ProfileListAdd {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		profileArrayListAddEnd();
+		//profileArrayListAddEnd();
 		//profileArrayListAddBeginning();
 		//profileLinkedListAddBeginning();
-		//profileLinkedListAddEnd();
+		profileLinkedListAddEnd();
 	}
 
 	/**
@@ -46,21 +45,66 @@ public class ProfileListAdd {
 	 * Characterize the run time of adding to the beginning of an ArrayList
 	 */
 	public static void profileArrayListAddBeginning() {
-		// TODO: FILL THIS IN!
+		Timeable timeable = new Timeable() {
+			List<String> list;
+
+			public void setup(int n) {
+				list = new ArrayList<String>();
+			}
+
+			public void timeMe(int n) {
+				for (int i=0; i<n; i++) {
+					list.add(0, "a string");
+				}
+			}
+		};
+		int startN = 4000;
+		int endMillis = 1000;
+		runProfiler("ArrayList add beginning", timeable, startN, endMillis);
 	}
 
 	/**
 	 * Characterize the run time of adding to the beginning of a LinkedList
 	 */
 	public static void profileLinkedListAddBeginning() {
-		// TODO: FILL THIS IN!
+		Timeable timeable = new Timeable() {
+			List<String> list;
+
+			public void setup(int n) {
+				list = new LinkedList<>();
+			}
+
+			public void timeMe(int n) {
+				for (int i=0; i<n; i++) {
+					list.add(0, "a string");
+				}
+			}
+		};
+		int startN = 128000;
+		int endMillis = 2000;
+		runProfiler("LinkedList add beginning", timeable, startN, endMillis);
 	}
 
 	/**
 	 * Characterize the run time of adding to the end of a LinkedList
 	 */
 	public static void profileLinkedListAddEnd() {
-		// TODO: FILL THIS IN!
+		Timeable timeable = new Timeable() {
+			List<String> list;
+
+			public void setup(int n) {
+				list = new LinkedList<>();
+			}
+
+			public void timeMe(int n) {
+				for (int i=0; i<n; i++) {
+					list.add("a string");
+				}
+			}
+		};
+		int startN = 64000;
+		int endMillis = 1000;
+		runProfiler("LinkedList add beginning", timeable, startN, endMillis);
 	}
 
 	/**
